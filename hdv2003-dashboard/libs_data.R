f71121b156146336f93d79742a082945ec1e1d15
Bervelin Lumesa <lumesabervelin@gmail.com>
2022-02-03 07:01:26 +0100

    Adding description

--- hdv2003-dashboard/libs_data.R
+++ hdv2003-dashboard/libs_data.R
@@ -15,11 +15,12 @@ library(DT)
 data("hdv2003")
 hdv2003 <- hdv2003[1:300, ]
 
-
+# names of qualitative variables
 quali <- hdv2003 %>% 
   select_if(is.factor) %>%
   names()
 
+# names of quantitative variables
 quanti <- hdv2003 %>% 
   select(-1) %>%
   select_if(is.numeric) %>%
